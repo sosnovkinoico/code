0ed116f8a0beefd30951b7a14cd3d4e1e3c39a26
Sosnovkino <social@sosnovkino.info>
2017-07-20 17:43:24 +0700

    Update iterable_mapping.sol

--- iterable_mapping.sol
+++ iterable_mapping.sol
@@ -1,5 +1,5 @@
+/// A bit modified library, taken from https://github.com/ethereum/dapp-bin/blob/master/library/iterable_mapping.sol
 /// @dev Models a uint -> uint mapping where it is possible to iterate over all keys. 
-/// https://github.com/ethereum/dapp-bin/blob/master/library/iterable_mapping.sol
 /// http://solidity.readthedocs.io/en/latest/types.html#mappings
 
 library IterableMappings
